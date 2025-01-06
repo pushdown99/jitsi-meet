24f81ea63771d382256a0c39c3874ba1d647426f
haeyeon, hwang <haeyun@gmail.com>
2025-01-06 13:47:51 +0900

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -239,7 +239,7 @@ function getDevServerConfig() {
                 warnings: false
             }
         },
-        host: '127.0.0.1',
+        host: '172.31.112.1',
         hot: true,
         proxy: [
             {
