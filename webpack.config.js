400f58118a0495b9c9e041c7118ca276c0ce2c7c
pushdown99 <haeyun@mgmail.com>
2025-01-21 09:17:33 +0900

    make

--- webpack.config.js
+++ webpack.config.js
@@ -239,7 +239,7 @@ function getDevServerConfig() {
                 warnings: false
             }
         },
-        host: '192.168.0.36',
+        host: '127.0.0.1',
         hot: true,
         proxy: [
             {
